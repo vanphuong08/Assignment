a4b1d17e9beb938120f5d9f97c8104d5e4938e5e
Văn Phương <levanphuong2608@gmail.com>
2020-10-30 15:46:49 +0700

    ones

---
+++ PRACTICAL/Context/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using PRACTICAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PRACTICAL.Context
+{
+    public class DataContext:DbContext
+    {
+        public DataContext() : base("connection") { }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
---
+++ PRACTICAL/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+
+namespace PRACTICAL.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phong_ban { get; set; }
+        public double tienluong { get; set; }
+    }
+}
